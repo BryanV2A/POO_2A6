7b0162b5038c17cf7f9afe4802c3ff57d48bfab5
Bryan <Alonsoval2.03@gmail.com>
2019-02-28 22:12:50 -0600

    feat: Se agregaron comentarios al programa

--- EsPar.py
+++ EsPar.py
@@ -2,12 +2,21 @@ class EsPar:
 
     __num = int(0)
     __resultado = int(0)
-    __r = ""
-
+    """
+    Solo se generaron dos valores porque fueron los unicos necesarios para la operacion
+    """
     def __init__(self, num):
+        """
+        Los parámetros del método constructor son solo
+        atributos de entrada y no de salida
+        """
         self.__num = num
 
     def determinarPar(self):
+        """
+        Método donde se determna si el número ingresado es par o no
+        mediante el uso del operador (%) que son los residuos en este caso de 2
+        """
         self.__resultado = self.__num % 2
 
         if(self.__resultado == 0):
@@ -15,3 +24,7 @@ class EsPar:
 
         if(self.__resultado != 0):
             return 0
+        """
+        Aqui se agregaron dos return que son valores de tipo booleanos 
+        para evitar el uso de mensajes dentro de los métodos
+        """
--- Resultado.py
+++ Resultado.py
@@ -1,8 +1,21 @@
+"""
+Para importar la clase EsPar es necesario agregar lo siguiente
+"""
 from EsPar import EsPar
-
-resultado = EsPar(5)
+"""
+from EsPar import EsPar = Desde el archivo EsPar importar la clase EsPar
+"""
+resultado = EsPar(12)
+"""
+nombre-del-objeto = nombre-de-la-clase(y, sus, parametros)
+"""
 print("Si el resultado es igual a 1 el numero es par")
 
 print ("Si el resultado es igual a 0 el numero es impar")
 
 print("El Resultado es = ", resultado.determinarPar())
+
+"""
+Para generar el resultado se agrega un mensaje con el objeto y este caso el metodo 
+donde contiene el rsultado
+"""
